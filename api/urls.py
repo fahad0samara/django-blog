6753c11dfa3053ede5e5dd44bec39a6c14d169b4
fahad <fah8ad@outlook.com>
2024-06-06 12:11:28 +0300

    add first commit

---
+++ api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns =[
+    path("", views.BlogPostListCreate.as_view(), name="blog"),
+    path("<int:pk>/", views.BlogPostDetail.as_view(), name="blog_detail")
+
+]
